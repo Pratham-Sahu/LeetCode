504154697963b2cbf0b7509d77402b6d8b9b72e1
Pratham Sahu <97285958+Pratham-Sahu@users.noreply.github.com>
2022-12-23 12:22:31 +0530

    Create Jewels and Stones.cpp

---
+++ Jewels and Stones.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count = 0;
+        for(int jewel = 0; jewel<jewels.length(); jewel++){
+            for(int stone = 0; stone < stones.length(); stone++){
+                if(jewels[jewel] == stones[stone]){
+                    count++;å
+                }
+            }
+        }
+    return count;
+    }
+};
